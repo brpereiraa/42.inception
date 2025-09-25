5aaebc0fb23291dde1007206ded72be3c682acdf
Bruno <brunoomiguel10@hotmail.com>
2025-09-25 23:49:50 +0100

    Working on .env file

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -2,29 +2,47 @@
 
 # Wait for MariaDB to be ready
 echo "Waiting for MariaDB to be ready..."
-while ! nc -z mariadb 3306; do
+while ! nc -z "$DB_HOST" 3306; do
   sleep 1
 done
 echo "MariaDB is ready!"
 
 cd /var/www/html
 
-# Download and setup WordPress only if not already done
 if [ ! -f wp-config.php ]; then
     echo "Setting up WordPress..."
+
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
+
     ./wp-cli.phar core download --allow-root
-    ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-    ./wp-cli.phar core install --url=localhost --title=testing --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-	./wp-cli.phar user create "USER" "USERMAIL" --role=author --user_pass=pass --allow-root
-    ./wp-cli.phar create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-    ./wp-cli.phar user create "USER" "USERMAIL" --role=author --user_pass=pass --allow-root
+
+    ./wp-cli.phar config create \
+        --dbname="$DB_NAME" \
+        --dbuser="$DB_USER" \
+        --dbpass="$DB_PASSWORD" \
+        --dbhost="$DB_HOST" \
+        --allow-root
+
+    ./wp-cli.phar core install \
+        --url="$DOMAIN" \
+        --title="testing" \
+        --admin_user="$ADMIN_USER" \
+        --admin_password="$ADMIN_PASSWORD" \
+        --admin_email="$ADMIN_EMAIL" \
+        --allow-root
+
+    ./wp-cli.phar user create \ 
+        "$WP_USER" \
+        "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role=author \
+        --allow-root
+
     echo "WordPress setup completed!"
 else
     echo "WordPress already configured, skipping setup..."
 fi
 
-# Start PHP-FPM
 echo "Starting PHP-FPM..."
 php-fpm8.2 -F
